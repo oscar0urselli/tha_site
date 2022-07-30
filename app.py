945e4e9580527f900007dad2294a49ffec62f373
oscar0urselli <urselli.oscar@gmail.com>
2022-07-30 20:00:49 +0200

    deploy heroku test1

--- app.py
+++ app.py
@@ -1,13 +1,6 @@
-import os
 import dotenv
 from flask import Flask, redirect, url_for, render_template, request, Response, abort
-import mysql.connector
-import time
 import mimetypes
-
-
-import utils.db
-import utils.auth
 
 
 
@@ -15,31 +8,10 @@ dotenv.load_dotenv()
 
 mimetypes.add_type('application/javascript', '.js')
 
-MYSQL_UP = True
-db  = None
-db_cursor = None
-
 
 app = Flask(__name__, static_folder = 'public')
 
 
-@app.before_first_request
-def init_db():
-    global db, db_cursor, MYSQL_UP
-    try:
-        db = mysql.connector.connect(
-            host = os.environ.get('DB_HOST'),
-            user = os.environ.get('DB_USER'),
-            password = os.environ.get('DB_PASSWORD'),
-            database = os.environ.get('DB_NAME')
-        )
-
-        db_cursor = db.cursor(buffered = True)
-
-        utils.db.check_db_integrity(db_cursor)
-    except mysql.connector.Error as e:
-        print(e)
-        MYSQL_UP = False
 
 @app.after_request
 def set_content_type(response):
@@ -60,26 +32,8 @@ def get_home():
 
 @app.route('/projects')
 def get_projects():
-    return Response('awnd', 400)
+    return render_template('projects.html')
 
 @app.route('/about')
 def get_about():
     return render_template('about.html')
-
-
-@app.route('/login', methods = ['GET', 'POST'])
-def login():
-    if request.method == 'GET':
-        return render_template('login.html')
-    else:
-        # email, password
-        data = request.form.to_dict()
-
-        msg, code = utils.auth.login(db, db_cursor, data)
-
-        return redirect(url_for('get_home'))
-        #return Response(response = msg, status = code, mimetype = 'text/plain')
-
-@app.route('/logout')
-def get_logout():
-    pass
